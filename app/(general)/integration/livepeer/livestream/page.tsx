07dba0d2f720dea49c2c3e0e24d0112ef76b1ceb
luizakp <luiza.puccinelli06@gmail.com>
2023-07-06 17:33:45 -0300

    refactor: use LinkComponent to navigate routes

--- app/(general)/integration/livepeer/livestream/page.tsx
+++ app/(general)/integration/livepeer/livestream/page.tsx
@@ -1,25 +1,28 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+
+import { LinkComponent } from '@/components/shared/link-component'
 
 export default function PageIntegration() {
-  const route = useRouter()
+  const newStreamPath = '/integration/livepeer/livestream/new'
+  const watchStreamPath = '/integration/livepeer/livestream/watch'
 
-  function handleWatchExistingLivestream() {
-    route.push('/integration/livepeer/livestream/watch')
-  }
-  function handleCreateNewLivestream() {
-    route.push('/integration/livepeer/livestream/new')
-  }
+  const [isLoadingNewStream, setIsLoadingNewStream] = useState<boolean>(false)
+  const [isLoadingExistingStream, setIsLoadingExistingStream] = useState<boolean>(false)
 
   return (
     <div className="card">
-      <button className="btn btn-emerald mt-4 w-full" onClick={handleCreateNewLivestream}>
-        Create a new livestream
-      </button>
-      <button className="btn btn-emerald mt-4 w-full" onClick={handleWatchExistingLivestream}>
-        Watch an existing livestream
-      </button>
+      <LinkComponent href={newStreamPath}>
+        <button className="btn btn-emerald mt-4 w-full" onClick={() => setIsLoadingNewStream(true)}>
+          {isLoadingNewStream ? 'Loading...' : 'Create a new livestream'}
+        </button>
+      </LinkComponent>
+      <LinkComponent href={watchStreamPath}>
+        <button className="btn btn-emerald mt-4 w-full" onClick={() => setIsLoadingExistingStream(true)}>
+          {isLoadingExistingStream ? 'Loading...' : 'Watch an existing livestream'}
+        </button>
+      </LinkComponent>
     </div>
   )
 }
--- app/(general)/integration/livepeer/vod/page.tsx
+++ app/(general)/integration/livepeer/vod/page.tsx
@@ -1,25 +1,28 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+
+import { LinkComponent } from '@/components/shared/link-component'
 
 export default function PageIntegration() {
-  const route = useRouter()
+  const newVodPath = '/integration/livepeer/vod/new'
+  const watchVodPath = '/integration/livepeer/vod/watch'
 
-  function handleWatchExistingVideo() {
-    route.push('/integration/livepeer/vod/watch')
-  }
-  function handleUploadNewVideo() {
-    route.push('/integration/livepeer/vod/new')
-  }
+  const [isLoadingNewVideo, setIsLoadingNewVideo] = useState<boolean>(false)
+  const [isLoadingExistingVideo, setIsLoadingExistingVideo] = useState<boolean>(false)
 
   return (
     <div className="card">
-      <button className="btn btn-emerald mt-4 w-full" onClick={handleUploadNewVideo}>
-        Upload a new video
-      </button>
-      <button className="btn btn-emerald mt-4 w-full" onClick={handleWatchExistingVideo}>
-        Watch an existing video
-      </button>
+      <LinkComponent href={newVodPath}>
+        <button className="btn btn-emerald mt-4 w-full" onClick={() => setIsLoadingNewVideo(true)}>
+          {isLoadingNewVideo ? 'Loading...' : 'Upload a new video'}
+        </button>
+      </LinkComponent>
+      <LinkComponent href={watchVodPath}>
+        <button className="btn btn-emerald mt-4 w-full" onClick={() => setIsLoadingExistingVideo(true)}>
+          {isLoadingExistingVideo ? 'Loading...' : 'Watch an existing video'}
+        </button>
+      </LinkComponent>
     </div>
   )
 }
