0c042a500051901c1f1e1020898a3b25f0d99419
luizakp <luiza.puccinelli06@gmail.com>
2023-07-10 14:41:00 -0300

    refactor: homepage redirect

--- app/(general)/integration/livepeer/page.tsx
+++ app/(general)/integration/livepeer/page.tsx
@@ -1,20 +1,4 @@
-'use client'
-
-import { useEffect } from 'react'
-
-import { useRouter } from 'next/navigation'
-
-import { Spinner } from '@/integrations/livepeer/components/spinner'
-
+import { redirect } from 'next/navigation'
 export default function PageIntegration() {
-  const route = useRouter()
-  useEffect(() => {
-    route.push('/integration/livepeer/vod')
-  }, [])
-
-  return (
-    <div className="flex h-full w-full items-center justify-center">
-      <Spinner />
-    </div>
-  )
+  redirect('/integration/livepeer/vod')
 }
