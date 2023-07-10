4c612126895ad169194b63aa0a4dae5c194df967
luizakp <luiza.puccinelli06@gmail.com>
2023-07-10 14:41:26 -0300

    fix: remove unnecessary validation on livepeer provider

--- integrations/livepeer/livepeer-provider.tsx
+++ integrations/livepeer/livepeer-provider.tsx
@@ -17,7 +17,7 @@ const livepeerTheme: ThemeConfig = {
 }
 
 export function LivepeerProvider({ children, customApiKey }: { children: ReactNode; customApiKey?: string }) {
-  const apiKey = customApiKey === undefined ? (env.NEXT_PUBLIC_LIVEPEER_API_KEY ? env.NEXT_PUBLIC_LIVEPEER_API_KEY : 'Initial Key') : customApiKey
+  const apiKey = customApiKey ?? (env.NEXT_PUBLIC_LIVEPEER_API_KEY ? env.NEXT_PUBLIC_LIVEPEER_API_KEY : 'Initial Key')
 
   if (!apiKey) {
     throw new Error('No Livepeer API key provided')
